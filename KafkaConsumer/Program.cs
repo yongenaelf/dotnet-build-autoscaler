2a17406840f585cf13017a556437e9f251a987c8
yongen.loong <yongen.loong@aelf.io>
2024-10-11 11:12:43 +0800

    feat: consumer and kafdrop

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Confluent.Kafka;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "kafka:9092",
+            GroupId = "build-consumer-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        
+        consumer.Subscribe("build_jobs");
+
+        try
+        {
+            while (true)
+            {
+                var cr = consumer.Consume();
+                Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e.Message}");
+            consumer.Close();
+        }
+    }
+}
